c394e72017ba40568512ebc38787cb7c4331261b
John Goodman <john@poeticsystems.com>
2021-02-09 16:10:24 -0600

    updated basic scrollmagic section wipes

---
+++ script.js
@@ -0,0 +1,21 @@
+$(function () { // wait for document ready
+		// init
+		var controller = new ScrollMagic.Controller();
+
+		// define movement of panels
+		var wipeAnimation = new TimelineMax()
+			.fromTo(".section.two",    1, {y:  "100%"}, {y: "0%", ease: Linear.easeNone})  // in from right
+			.fromTo(".section.three", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
+			.fromTo(".section.four", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
+
+		// create scene to pin and link animation
+		new ScrollMagic.Scene({
+				triggerElement: ".sections",
+				triggerHook: "onLeave",
+				duration: "300%"
+			})
+			.setPin(".sections")
+			.setTween(wipeAnimation)
+			.addIndicators() // add indicators (requires plugin)
+			.addTo(controller);
+	});
